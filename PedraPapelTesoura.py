57bcda729fc2a16f13a4f3fec3b9c2e469a37e38
Pedro Henrique Muniz Araujo <90574929+Pedrucs@users.noreply.github.com>
2022-10-22 22:38:25 -0300

    Update PedraPapelTesoura.py

--- PedraPapelTesoura.py
+++ PedraPapelTesoura.py
@@ -13,23 +13,23 @@ print(f"\nVocê Escolheu {user_action}, A máquina escolheu {computer_action}.\n
 #DETERMINANDO O VENCEDOR
 
 if user_action == computer_action:
-    print(f"Ambos os jogadores escolheram {user_action}. É um empate!")
+    print(f"Ambos os jogadores escolheram {user_action}. Empatou :0 ")
+    
 elif user_action == "pedra":
     if computer_action == "tesoura":
-        print("Pedra esmaga a tesoura! Você ganhou!")
+        print("Pedra esmaga a tesoura! Você ganhou! ")
     else:
-        print("Papel cobre a pedra! Você perdeu :(")
+        print("Papel cobre a pedra! Você perdeu :( ")
+        
 elif user_action == "papel":
     if computer_action == "pedra":
-        print("Papel cobre a pedra! Você ganhou!")
+        print("Papel cobre a pedra! Você ganhou! ")
     else:
-        print("Tesoura corta o papel! Você perdeu :(")
-elif user_action == "tesoura":
+        print("Tesoura corta o papel! Você perdeu :( ")
+
+        elif user_action == "tesoura":
     if computer_action == "pedra":
-        print("Tesoura corta o papel! Você ganhou!")
+        print("Tesoura corta o papel! Você ganhou! ")
     else:
-        print("Pedra esmaga a tesoura! Você perdeu :(")
-
-
-
-
+        print("Pedra esmaga a tesoura! Você perdeu :( ")
+        
